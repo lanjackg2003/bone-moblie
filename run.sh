b6e2411d7f90c1607b41848345604786d380b35d
jack <lanjackg2003@qq.com>
2018-05-18 16:29:40 +0800

    support native debug

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop BoneMobile 
 docker rm BoneMobile
-docker run -ti -v  `pwd`:/build -p 9000:9000 --name BoneMobile jacklan/bone-mobile
+docker run -ti -v  `pwd`:/build -p 9000:9000 -p 8081:8081 --name BoneMobile jacklan/bone-mobile
--- win_run.bat
+++ win_run.bat
@@ -1,4 +1,4 @@
 docker stop BoneMobile 
 docker rm BoneMobile 
 ::docker run -ti -v  %cd%:/build --name BoneMobile jacklan/bone-mobile /build/login.sh
-docker run -ti -v  %cd%:/build  -p 9000:9000 --name BoneMobile jacklan/bone-mobile
+docker run -ti -v  %cd%:/build  -p 9000:9000 -p 8081:8081 --name BoneMobile jacklan/bone-mobile
