c0543567e7597ee7a3d3809a376d7ddd2890bd99
jack <lanjackg2003@qq.com>
2018-05-18 10:08:59 +0800

    mapping the port 9000 to host

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop BoneMobile 
 docker rm BoneMobile
-docker run -ti -v  %cd%:/build --name BoneMobile jacklan/bone-mobile
+docker run -ti -v  `pwd`:/build -p 9000:9000 --name BoneMobile jacklan/bone-mobile
